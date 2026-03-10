8d37db5666a018d8545f1ccc399c55dccb68e859
Noam Schmidiger <noam.schmidige@icloud.com>
2026-03-10 08:32:22 +0100

    Created Spring Boot Project

---
+++ bibliothek/bibliothek/src/main/java/com/bbzbl/bibliothek/BibliothekApplication.java
@@ -0,0 +1,13 @@
+package com.bbzbl.bibliothek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothekApplication.class, args);
+	}
+
+}
---
+++ bibliothek/bibliothek/src/test/java/com/bbzbl/bibliothek/BibliothekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbzbl.bibliothek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
